a9df604bc4556abb12f5db9d49ca2eb91df2295b
AbhineetK <68178267+AbhiK002@users.noreply.github.com>
2022-03-31 14:25:36 +0530

    Update bounce.py

--- bounce.py
+++ bounce.py
@@ -6,26 +6,48 @@ class App:
         self.master = Tk()
         self.master.title("Error")
 
-        self.window_width = 360
-        self.window_height = 180
-        self.x_limit = self.master.winfo_screenwidth() - self.window_width
-        self.y_limit = self.master.winfo_screenheight() - self.window_height
-        self.window_coord_x = int(self.x_limit/2)
-        self.window_coord_y = int(self.y_limit/2)
+        self.WINDOW_W = 360
+        self.WINDOW_H = 180
+        self.x_limit = self.master.winfo_screenwidth() - self.WINDOW_W
+        self.y_limit = self.master.winfo_screenheight() - self.WINDOW_H
 
-        self.bounce_factor = 0.4
+        self.GRAVITY = 10  # px/20ms^2
+        self.BOUNCE_FACTOR = 0.7  # percentage of velocity remaining right after bounce
+        self.curr_vel = 0  # px/20ms
 
-        self.master.geometry(f"{self.window_width}x{self.window_height}+"
-                             f"{self.window_coord_x}+{self.window_coord_y}")
+        self.held = False
+        self.master.geometry(f"{self.WINDOW_W}x{self.WINDOW_H}+"
+                             f"{int(self.x_limit / 2)}+{int(self.y_limit / 2)}")
 
-        
+        self.master.bind("<Button-1>", lambda e: self.under_hold())
+        self.master.bind("<ButtonRelease-1>", lambda e: self.released())
 
-        self.gravity = 10
+        self.master.after(1000, self.move)
 
         self.master.mainloop()
 
-    def fall(self):
-        pass
+    def under_hold(self):
+        self.curr_vel = 0
+        self.held = True
+
+    def released(self):
+        self.held = False
+
+    def ypos(self):
+        return self.master.winfo_y()
+
+    def move(self, event=None):  # moves the window
+        if not self.held:
+            if self.ypos() < self.y_limit:
+                self.curr_vel += self.GRAVITY
+                self.master.geometry(f"+{self.master.winfo_x()}+{self.ypos()+self.curr_vel}")
+            else:
+                self.master.geometry(f"+{self.master.winfo_x()}+{self.y_limit}")
+                if self.curr_vel != 0:
+                    self.curr_vel = int((-1)*(self.curr_vel * self.BOUNCE_FACTOR))
+                    self.master.geometry(f"+{self.master.winfo_x()}+{self.ypos() + self.curr_vel}")
+
+        self.master.after(20, self.move)
 
 
 if __name__ == '__main__':
