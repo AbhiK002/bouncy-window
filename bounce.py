ecd75c13d8e07b2da63567dfa9a55d1bf1b45830
AbhineetK <68178267+AbhiK002@users.noreply.github.com>
2022-03-31 18:47:23 +0530

    Update bounce.py

--- bounce.py
+++ bounce.py
@@ -11,8 +11,8 @@ class App:
         self.x_limit = self.master.winfo_screenwidth() - self.WINDOW_W
         self.y_limit = self.master.winfo_screenheight() - self.WINDOW_H
 
-        self.GRAVITY = 10  # px/20ms^2
-        self.BOUNCE_FACTOR = 0.7  # percentage of velocity remaining right after bounce
+        self.GRAVITY = 4  # px/20ms^2
+        self.BOUNCE_FACTOR = 0.42  # percentage of velocity remaining right after bounce
         self.curr_vel = 0  # px/20ms
 
         self.held = False
@@ -37,15 +37,19 @@ class App:
         return self.master.winfo_y()
 
     def move(self, event=None):  # moves the window
-        if not self.held:
+        if self.curr_vel >= 0:
             if self.ypos() < self.y_limit:
                 self.curr_vel += self.GRAVITY
                 self.master.geometry(f"+{self.master.winfo_x()}+{self.ypos()+self.curr_vel}")
             else:
                 self.master.geometry(f"+{self.master.winfo_x()}+{self.y_limit}")
-                if self.curr_vel != 0:
-                    self.curr_vel = int((-1)*(self.curr_vel * self.BOUNCE_FACTOR))
-                    self.master.geometry(f"+{self.master.winfo_x()}+{self.ypos() + self.curr_vel}")
+                if self.curr_vel >= self.GRAVITY:
+                    self.curr_vel = int(self.curr_vel * (-1) * self.BOUNCE_FACTOR)
+                else:
+                    self.curr_vel = 0
+        else:
+            self.curr_vel += self.GRAVITY
+            self.master.geometry(f"+{self.master.winfo_x()}+{self.ypos() + self.curr_vel}")
 
         self.master.after(20, self.move)
 
